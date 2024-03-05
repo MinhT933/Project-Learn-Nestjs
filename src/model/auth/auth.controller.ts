69913178310a2e6cbdb9a248960ff8bb4b5afcfc
MinhT <phammanhtoanhht933@gmail.com>
2024-03-05 13:38:49 +0700

    add code

--- src/model/auth/auth.controller.ts
+++ src/model/auth/auth.controller.ts
@@ -18,24 +18,30 @@ export class AuthController {
   async signUp(@Body() body: UserDTO) {
     return this.authService.SignUp(body);
   }
-  
+
   @Public()
   @Post('login')
   async login(
     @Body() body: AuthDTO,
     @Res({ passthrough: true }) response: Response,
   ) {
-    const { access_token } = await this.authService.login(body);
-    console.log('login oke');
+    try {
+      const { access_token } = await this.authService.login(body);
 
-    response
-      .cookie('access_token', access_token, {
-        httpOnly: true, //cross-site scripting (XSS) attacks
-        secure: false, //sent over HTTPS connections
-        sameSite: 'lax',
-        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-      })
-      .send({ status: 'ok' });
+      response
+        .cookie('access_token', access_token, {
+          httpOnly: true,
+          secure: false,
+          sameSite: 'lax',
+          path: '/',
+          maxAge: 3600000,
+          expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        })
+        .send({ status: 'ok', access_token });
+    } catch (error) {
+      console.error('Error setting cookie:', error);
+      response.status(500).send({ error: 'Internal Server Error' });
+    }
   }
 
   @Get('me')
